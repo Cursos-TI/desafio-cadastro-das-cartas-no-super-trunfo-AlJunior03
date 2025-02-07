4912851a2187894fc2689515693a02c07eeec99e
AlJunior03 <junior.27699@gmail.com>
2025-02-07 12:57:13 +0000

    Cadastro das cartas

---
+++ SuperTrunfo.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+// Desafio Super Trunfo - Países
+// Tema 1 - Cadastro das Cartas
+
+int main{()
+
+char Estado; // Estado da cidade (A - H)
+int Cidade; // Número da cidade (1, 2, 3, 4)
+int População; 
+float Area;
+float PIB;
+int Nº de pontos turisticos;
+
+// Cadastro das informaçoes das cartas
+
+printf("Estado (A-H): \n");
+scanf("%c" &Estado);
+printf("Estado: %c\n" , Estado);
+
+printf("Cidade (1-4): \n");
+scanf("%d", &Cidade);
+printf("Cidade: %d\n" , Cidade);
+
+printf("População: \n");
+scanf("%d", &População);
+printf("População: %d\n" , População),
+
+printf("Area: \n");
+scanf("%f", &Area);
+printf("Area: %f\n" , Area);
+
+printf("PIB: \n");
+scanf("%f", &PIB);
+printf("PIB: %f\n" , PIB);
+
+printf("Nº de pontos turisticos: \n");
+scanf("%c" &Nº de pontos turisticos);
+printf("Nº de pontos turisticos: %c\n" , Nº de pontos turisticos);
+
+
+
+
+
+
+
+
+
+return 0;
+
+}
